196abf95c0309493019f57122e7ca388d3472a4d
Mamadou Yaya Diallo <ydiallogn@gmail.com>
2024-10-20 21:26:16 +0000

    api documentation done using openApi

--- src/main/java/com/example/product_management/config/SecurityConfiguration.java
+++ src/main/java/com/example/product_management/config/SecurityConfiguration.java
@@ -25,7 +25,7 @@ public class SecurityConfiguration {
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/v1/test/users/auth/**")
+                .requestMatchers("/api/v1/test/users/auth/**", "/v3/api-docs/**", "/swagger-ui/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
